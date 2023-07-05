baca72063af77cd23a683ce7ded75bd005c8adad
Markus Becker <markus.becker@cloud-plant.com>
2023-07-05 15:14:09 +0200

    Added something

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,6 +3,7 @@ resource "aws_iam_role" "aft_states" {
   assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", { none = "none" })
 }
 
+
 resource "aws_iam_role_policy" "aft_states" {
   name = "aft-account-provisioning-customizations-policy"
   role = aws_iam_role.aft_states.id
